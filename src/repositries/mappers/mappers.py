0ca7bac4693bff29c2582a06d1a0ae01834420d3
heist <heist2000@gmail.com>
2025-04-12 14:29:53 +0300

    update datamapper

--- src/repositries/mappers/mappers.py
+++ src/repositries/mappers/mappers.py
@@ -1,15 +1,59 @@
+from typing import Any
+from pydantic import BaseModel
+from sqlalchemy.orm import DeclarativeBase
+
 from models.services import ServicesORM
 from models.users import UsersORM
-from repositries.mappers.base import DataMapper
 from schemas.services import ServiceRead
 from schemas.users import UserRead
 
 
-class ServicesDataMapper(DataMapper):
+class BaseDataMapper:
+    db_model: DeclarativeBase = None  ## модель sqlalchemy
+    schema: BaseModel = None  ## pydantic схема
+
+    @classmethod
+    def map_to_domain_entity(cls, model: DeclarativeBase) -> BaseModel:
+        """Конвертирует `sqlalchemy` модель в `pydantic` схему
+
+        Args:
+            model (DeclarativeBase): данные в формате `sqlalchemy` модели. Например `<UsersORM: id=15, name=Sanek>`
+
+        Returns:
+            BaseModel: данные в формате `pydantic` схемы. Например `UserRead(id=15, name='Sanek')`
+        """
+        return cls.schema.model_validate(model, from_attributes=True)
+
+    @classmethod
+    def map_to_dict(cls, model: DeclarativeBase) -> dict[str, Any]:
+        """Конвертирует `sqlalchemy` модель в `pydantic` схему, а затем в словарь
+
+        Args:
+            model (DeclarativeBase): данные в формате `sqlalchemy` модели. Например `<UsersORM: id=15, name=Sanek>`
+
+        Returns:
+            dict[str, Any]: словарь значений. Например `{'id': 15, 'name': 'Sanek'}`
+        """
+        return cls.schema.model_validate(model, from_attributes=True).model_dump()
+
+    @classmethod
+    def map_to_presistence_entity(cls, data: BaseModel) -> DeclarativeBase:
+        """Конвертирует данные `pydantic` схемы в модель `sqlalchemy`
+
+        Args:
+            data (BaseModel): данные в формате `pydantic` схемы. Например `UserRead(id=15, name='Sanek')`
+
+        Returns:
+            DeclarativeBase: данные в формате `sqlalchemy` модели. Например `<UsersORM: id=15, name=Sanek>`
+        """
+        return cls.db_model(**data.model_dump())
+
+
+class ServicesDataMapper(BaseDataMapper):
     db_model = ServicesORM
     schema = ServiceRead
 
 
-class UsersDataMapper(DataMapper):
+class UsersDataMapper(BaseDataMapper):
     db_model = UsersORM
     schema = UserRead
